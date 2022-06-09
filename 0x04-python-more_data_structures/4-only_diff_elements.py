762f45734768634a83a8fe8ad3c632fa0832d220
Pascalchinedu <pascalchinedu2000@gmail.com>
2022-06-09 07:46:26 -0700

    More data structures

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-def only_diff_elements(set_1, set_2)
-    return (set_1 ^ set_2)
+def only_diff_elements(set_1, set_2):
+    result_list = set(set_1) ^ set(set_2)
+    return result_list
