31a76cb6354c2338083723f5348be196f0b9ceea
Pascalchinedu <pascalchinedu2000@gmail.com>
2022-06-28 01:31:27 -0700

    Test Driven Developmemt

---
+++ 0x07-python-test_driven_development/102-python.c
@@ -0,0 +1,46 @@
+
+Chioma2000
+/
+alx-higher_level_programming
+Public
+Code
+Issues
+Pull requests
+Actions
+Projects
+Wiki
+Security
+Insights
+alx-higher_level_programming/0x07-python-test_driven_development/102-python.c
+@Chioma2000
+Chioma2000 tests
+ 1 contributor
+28 lines (23 sloc)  625 Bytes
+#include "Python.h"
+
+/**
+ * print_python_string - Prints information about Python strings.
+ * @p: A PyObject string object.
+ */
+void print_python_string(PyObject *p)
+{
+	long int length;
+
+	fflush(stdout);
+
+	printf("[.] string object info\n");
+	if (strcmp(p->ob_type->tp_name, "str") != 0)
+	{
+		printf("  [ERROR] Invalid String Object\n");
+		return;
+	}
+
+	length = ((PyASCIIObject *)(p))->length;
+
+	if (PyUnicode_IS_COMPACT_ASCII(p))
+		printf("  type: compact ascii\n");
+	else
+		printf("  type: compact unicode object\n");
+	printf("  length: %ld\n", length);
+	printf("  value: %ls\n", PyUnicode_AsWideCharString(p, &length));
+}
