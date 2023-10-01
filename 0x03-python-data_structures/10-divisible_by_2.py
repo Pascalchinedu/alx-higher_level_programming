1f8c1cdee07d6a8ad476ef71e2333acac0a36b70
Pascal <pascalchinedu2000@gmail.com>
2023-10-01 16:17:17 +0100

    function that finds all multiple of 2 and appends True or False if number is divisible

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,11 +1,25 @@
 #!/usr/bin/python3
+
+'''
+A function that finds all multiples of 2 in a list.
+Returns a new list with True or False, depending on whether the interger at the same position in the original list is a multiple of 2
+The new list should have the same original size as the original list
+'''
 def divisible_by_2(my_list=[]):
-    check_div = []
 
+    new_list = []
+    # iterates through length of list
     for i in range(len(my_list)):
-        if my_list[i] % 2 == 0:
-            check_div.append(True)
+        # checks if current integer is divisible by 2
+        if i % 2 == 0:
+            new_list.append(True)
         else:
-            check_div.append(False)
+            new_list.append(False)
 
-    return (check_div)
+    return new_list
+
+# example usage 
+my_list = [0, 1, 2, 3, 4, 5, 6]
+
+print(my_list) # should return [True, False, True, False, True, False, True]
+print(divisible_by_2(my_list))
