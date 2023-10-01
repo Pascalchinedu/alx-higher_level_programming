ce109f8ea538453e4d268b5c4a180ecbb0ef685d
Pascal <pascalchinedu2000@gmail.com>
2023-10-01 15:47:50 +0100

    a function that retrieves an element from a list

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
+
+# a fuction that retrieves an element from a list 
 def element_at(my_list, idx):
+
     if idx < 0:
         return (None)
-
-    length = len(my_list)
-
-    if idx > length - 1:
+    elif idx > len(my_list) - 1:
         return (None)
+    else:
+        return (my_list[idx])
+    
 
-    return(my_list[idx])
+# Example        
+my_list = [1, 2, 3, 4, 6]
+idx = 5
+print("Element at index {:d} is {}".format(idx, element_at(my_list, idx)))
+# Should print: Element at index 4 is 6
