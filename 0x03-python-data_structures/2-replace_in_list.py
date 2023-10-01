02ca3079d689ac5a143ea1a5c399275a888d2129
Pascal <pascalchinedu2000@gmail.com>
2023-10-01 15:50:12 +0100

    function retrieves an element of a list at a specific position

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,13 +1,24 @@
 #!/usr/bin/python3
+
+# A function that replaces an element of a list at a specific position
 def replace_in_list(my_list, idx, element):
+
     if idx < 0:
         return (my_list)
-
-    length = len(my_list)
-
-    if idx > length - 1:
+    
+    elif idx > len(my_list) - 1:
+        return (my_list)
+    
+    else:
+        my_list[idx] = element
         return (my_list)
 
-    my_list[idx] = element
 
-    return (my_list)
+# Example Usage
+my_list = [1, 2, 3, 4, 5]
+idx = 4
+new_element = 9
+new_list = replace_in_list(my_list, idx, new_element)
+
+print("{}".format(new_list))
+print("{}".format(my_list))
