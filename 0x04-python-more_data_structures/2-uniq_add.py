40fac0161277fe4e5a30d635ccc1f067dd85c316
Pascal <pascalchinedu2000@gmail.com>
2023-10-02 17:14:47 +0100

    added example usage to the code

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+
+'''
+    A function that adds all unique integers in a list
+    (only once for each integers)
+'''
 def uniq_add(my_list=[]):
     uniq_list = set(my_list)
     num = 0
@@ -7,3 +12,9 @@ def uniq_add(my_list=[]):
         num += i
 
     return (num)
+
+
+# example usage
+my_list = [1, 2, 3, 1, 4, 2, 5, 12, 1, 4, 5, 6, 6, 8, 5, 9]
+result = uniq_add(my_list)
+print("Result: {:d}".format(result))
