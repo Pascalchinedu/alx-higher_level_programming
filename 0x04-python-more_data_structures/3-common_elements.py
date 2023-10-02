29a56ceb46e92c4a82e931ab1ff9f92e88158c9b
Pascal <pascalchinedu2000@gmail.com>
2023-10-02 17:17:55 +0100

    comment added describing function

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
+
+# A function that returns a set of common elements in two sets.
 def common_elements(set_1, set_2):
     return (set_1 & set_2)
+
+
+# Example
+set_1 = { "Python", "C", "Javascript" }
+set_2 = { "Bash", "C", "Ruby", "Perl" }
+c_set = common_elements(set_1, set_2)
+print(sorted(list(c_set)))
